83ca43ae7a279918e8501f4d4585c1368fb43efb
Philip Welber <pwelber@targetedsocial.com>
2016-03-14 14:11:29 -0400

    completes ad pulling and rendering

--- client/controllers/ads.js
+++ client/controllers/ads.js
@@ -7,34 +7,57 @@ Tracker.autorun(function () {
 });
 
 Template.ads.helpers({
-    'getAds': function () {
-      console.log('checking for ads');
-      let campaignNumber = FlowRouter.current().params.campaign_id;
-      let ad = Ads.findOne({'data.campaign_id': campaignNumber});
-      if (!ad) {
-        console.log('need to fetch ads')
-        Meteor.call('getAds', campaignNumber)
-      } else {
-        console.log("you should be seeing ads");
-        ads = Ads.find({'data.campaign_id': campaignNumber});
-        try {
-          // TODO control flow to handle single ad, set of ads, carousel
-          // maybe do by length or mongo Array or by property / key name
-          ads.data.cpc = mastFunc.money(ads.data.cpc);
-          ads.data.cpm = mastFunc.money(ads.data.cpm);
-          ads.data.cpp = mastFunc.money(ads.data.cpp);
-          ads.data.cost_per_page_engagement = mastFunc.money(ads.data.cost_per_page_engagement
-          );
-          ads.data.cost_per_post_engagement = mastFunc.money(ads.data.cost_per_post_engagement);
-        } catch (e) {
-          console.log(e)
-        } finally {
-          return ads
+  'getAds': function () {
+    let campaignNumber = FlowRouter.current().params.campaign_id;
+    let ad = Ads.findOne({'data.campaign_id': campaignNumber});
+    if (!ad) {
+      console.log('need to fetch ads');
+      var target = document.getElementById("spinner-div");
+      let spun = Blaze.render(Template.spin, target);
+      Meteor.call('getAds', campaignNumber, function (error, result) {
+        if (result) {
+          Blaze.remove(spun);
         }
+      });
+    } else {
+      console.log("you should be seeing ads");
+      ads = Ads.find({'data.campaign_id': campaignNumber}).fetch();
+      try {
+        // TODO control flow to handle single ad, set of ads, carousel
+        // maybe do by length or mongo Array or by property / key name
+        if (ads.length === 1) {
+          ads[0].data.cpc = mastFunc.money(ads[0].data.cpc);
+          ads[0].data.cpm = mastFunc.money(ads[0].data.cpm);
+          ads[0].data.cpp = mastFunc.money(ads[0].data.cpp);
+          ads[0].data.cost_per_page_engagement = mastFunc.money(ads[0].data.cost_per_page_engagement);
+          ads[0].data.cost_per_post_engagement = mastFunc.money(ads[0].data.cost_per_post_engagement);
+          ads[0].data.cost_per_link_click = mastFunc.money(ads[0].data.cost_per_link_click);
+          if (ads[0].data.video_view > 1) {
+            ads[0].data.cost_per_video_view = mastFunc.money(ads[0].data.cost_per_video_view);
+            ads[0].data.cost_per_video_play = mastFunc.money(ads[0].data.cost_per_video_play);
+          }
+        } else if (ads.length > 1) {
+          ads.forEach(el => {
+            el.data.cpc = mastFunc.money(el.data.cpc);
+            el.data.cpm = mastFunc.money(el.data.cpm);
+            el.data.cpp = mastFunc.money(el.data.cpp);
+            el.data.cost_per_page_engagement = mastFunc.money(el.data.cost_per_page_engagement);
+            el.data.cost_per_post_engagement = mastFunc.money(el.data.cost_per_post_engagement);
+            el.data.cost_per_link_click = mastFunc.money(el.data.cost_per_link_click);
+              if (el.data.video_view) {
+                el.data.cost_per_video_view = mastFunc.money(el.data.cost_per_video_view);
+              }
+          });
+        }
+      } catch (e) {
+        console.log(e)
+      } finally {
+        return ads
       }
-    },
-    'getCampaignNumber': function () {
-        let campaignNumber = FlowRouter.current().params.campaign_id;
-        return campaignNumber;
     }
+  },
+  'getCampaignNumber': function () {
+      let campaignNumber = FlowRouter.current().params.campaign_id;
+      return campaignNumber;
+  }
 });
--- server/methods/ads.js
+++ server/methods/ads.js
@@ -7,6 +7,7 @@ Meteor.methods({
 
 Meteor.methods({
   'getAds': function (accountNumber) {
+    console.log("getAds running");
     let adsArray = [];
     let otherArray = [];
     let masterArray = [];
@@ -22,7 +23,8 @@ Meteor.methods({
           let attachments = {}
           // make 2nd api call with object_story_id to retrieve attachments
           let attachment = HTTP.call('GET', 'https://graph.facebook.com/v2.5/'+el.adcreatives.data[0].object_story_id+'?fields=attachments,message&access_token='+token+'', {});
-          if (attachment.data.attachments.data[0].hasOwnProperty('subattachments')) { //essentially, is this a carousel ad?
+          if (attachment.data.attachments.data[0].hasOwnProperty('subattachments')) {
+          // essentially, is this a carousel ad?
             attachment.data.attachments.data[0].subattachments.data.forEach((element,index) => {
               let carouselAttachments = {};
               carouselAttachments['src'] = element.media.image.src;
@@ -36,6 +38,7 @@ Meteor.methods({
             otherArray.push(el)
           } else {
             let obj = attachment.data.attachments.data[0];   // for readability purposes
+            attachments['message'] = attachment.data.message;
             attachments['description'] = obj.description;
             attachments['url'] = obj.target.url;
             attachments['picture'] = obj.media.image.src;
@@ -50,47 +53,48 @@ Meteor.methods({
         otherArray.forEach(el => {
           data = {};
           for (let key in el) {
-              if (key === "insights") {
-                  // console.log(el[key]);
-                  el[key].data.forEach(el => {
-                      for (let k in el) {
-                          if (k === "actions") {
-                              el[k].forEach(el => {
-                                  // this check looks for a period in the key name and
-                                  // replaces it with an underscore if found
-                                if (/\W/g.test(el.action_type)) {
-                                  el.action_type = el.action_type.replace(/\W/g, "_");
-                                  data[el.action_type] = el.value;
-                                }
-                                  data[el.action_type] = el.value;
-                              });
-                            } else if (k === "cost_per_action_type") {
-                              el[k].forEach(el => {
-                                 if (/\W/g.test(el.action_type)) {
-                                     el.action_type = el.action_type.replace(/\W/g, "_");
-                                     data["cost_per_"+el.action_type] = el.value;
-                                  } else {
-                                     data["cost_per_"+el.action_type] = el.value;
-                                 }
-                             });
-                         } else if (k === "website_ctr") {
-                             el[k].forEach(el => {
-                                 data[el.action_type+"_ctr"] = el.value;
-                             });
-                         } else {
-                            if (/\W/g.test(k)) {
-                                  k = k.replace(/\W/g, "_");
-                                 data[k] = el[k];
-                            } else {
-                                 data[k] = el[k]
-                             }
-                         }
+            if (key === "insights") {
+              // console.log(el[key]);
+              el[key].data.forEach(el => {
+                for (let k in el) {
+                  if (k === "actions") {
+                    el[k].forEach(el => {
+                        // this check looks for a period in the key name and
+                        // replaces it with an underscore if found
+                      if (/\W/g.test(el.action_type)) {
+                        el.action_type = el.action_type.replace(/\W/g, "_");
+                        data[el.action_type] = el.value;
                       }
-                  });
-              }
+                        data[el.action_type] = el.value;
+                    });
+                  } else if (k === "cost_per_action_type") {
+                    el[k].forEach(el => {
+                     if (/\W/g.test(el.action_type)) {
+                         el.action_type = el.action_type.replace(/\W/g, "_");
+                         data["cost_per_"+el.action_type] = el.value;
+                      } else {
+                         data["cost_per_"+el.action_type] = el.value;
+                      }
+                   });
+                  } else if (k === "website_ctr") {
+                   el[k].forEach(el => {
+                       data[el.action_type+"_ctr"] = el.value;
+                   });
+                  } else {
+                    if (/\W/g.test(k)) {
+                      k = k.replace(/\W/g, "_");
+                      data[k] = el[k];
+                    } else {
+                       data[k] = el[k]
+                    }
+                  }
+                }
+              });
             }
+          }
           // check for carouselData
           if (el.attachments) {
+            data['message'] = el.attachments.message;
             data['description'] = el.attachments.description;
             data['url'] = el.attachments.url;
             data['picture'] = el.attachments.picture;
@@ -107,19 +111,6 @@ Meteor.methods({
 
           masterArray.push(data);
         });
-        // console.log(data);
-        console.log(masterArray);
-
-            // while (true) {
-            //     try {
-            //         ads = HTTP.call('GET', ads.data.paging['next'], {});
-            //         adsArray.push(ads.data.data)
-            //     } catch(e) {
-            //         console.log('no more pages or error in while true loop', e);
-            //         break;
-            //     }
-            // }
-
         } catch(e) {
             console.log('Error pulling Ads data', e);
         }
@@ -134,7 +125,6 @@ Meteor.methods({
         } finally {
           return "success!"
         }
-
     }
 });
 
