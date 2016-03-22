0bf5ef8113100f0860264a3c67da145ab1c319a4
Philip Welber <pwelber@targetedsocial.com>
2016-03-22 17:56:59 -0400

    continues with chartjs and ran into tracker autoflush issue

--- client/controllers/charts.js
+++ client/controllers/charts.js
@@ -1,3 +1,6 @@
+// var range = require('moment-range');
+
+
 Tracker.autorun(function () {
   if (FlowRouter.subsReady('campaignInsightList')) {
     console.log('campaignInsights subs ready!');
@@ -10,6 +13,12 @@ Tracker.autorun(function () {
 let chart = Template.charts;
 
 chart.onRendered( function (){
+  let dailyBreakdown = InsightsBreakdownsByDays.find(
+    {'data.campaign_id': FlowRouter.current().params.campaign_id}
+  ).fetch();
+  const initiative = Initiatives.findOne(
+        {"campaign_names": {$in: [dailyBreakdown[0].data.campaign_name]}
+        });
   var ctx  = document.getElementById("myChart").getContext("2d");
 
   // Set the options
@@ -19,11 +28,11 @@ chart.onRendered( function (){
     //String - Colour of the grid lines
     scaleGridLineColor: "rgba(0,0,0,.05)",
     //Number - Width of the grid lines
-    scaleGridLineWidth: 1,
+    // scaleGridLineWidth: 1,
     //Boolean - Whether to show horizontal lines (except X axis)
     scaleShowHorizontalLines: true,
     //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
+    scaleShowVerticalLines: false,
     //Boolean - Whether the line is curved between points
     bezierCurve: false,
     //Number - Tension of the bezier curve between points
@@ -43,48 +52,80 @@ chart.onRendered( function (){
     //Boolean - Whether to fill the dataset with a colour
     datasetFill: true,
     //String - A legend template
-    // legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+    // showXLabels: 25,
+    // scale: true,
+    // scaleOverride: true,
+    // scaleSteps: 10,
+    // scaleStepWidth: 31,
+    // scaleStartValue: 0,
+    // Boolean - If we want to override with a hard coded x scale
+    xScaleOverride: true,
+     // ** Required if scaleOverride is true **
+     // Number - The number of steps in a hard coded x scale
+    xScaleSteps:30,
+
+     // Number - The value jump in the hard coded x scale
+    xScaleStepWidth: 48 * 3600000, // 24 is 1 day, 48 is two days
+
+     // Number - The x scale starting value
+    xScaleStartValue: Date.parse(initiative.startDate)
+    // xScaleStartValue: 0
   };
 
   // Get data from collections
-  let dailyBreakdown = InsightsBreakdownsByDays.find({'data.campaign_id': FlowRouter.current().params.campaign_id}).fetch();
-  let labels = [];
-  dailyBreakdown.forEach(el => {
-    labels.push(moment(el.data.date_start, "MM-DD").format("MM-DD"));
-  });
+  let labels = []; // this will be the date range
 
+  let timeFormat = "MM-DD-YYYY";
+  let days = moment(initiative.endDate, timeFormat).diff(moment(initiative.startDate, timeFormat), 'days');
 
+  // var start = new Date(initiative.startDate);
+  // var end   = new Date(initiative.endDate);
+  // var dr    = moment.range(start, end);
+  // var arrayOfDays = dr.toArray('days');
+  // arrayOfDays.forEach(el => {
+  //   labels.push(moment(el).format("MM-DD"))
+  // });
+
+  let avg = initiative.quantity / days;
+  let data1 = [];
+  let total = 0;
+  for (let i = 0; i < days; i++) {
+    data1.push(total);
+    total = total + avg;
+  }
+  // console.log('data1', data1)
 
 
 
 
   // Set the data
   var data = {
-    labels: labels,
+    // labels: labels,
+    // labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
     datasets: [{
       label: "Evenly Distributed Output",
       fillColor: "rgba(220,220,220,0)",
-      strokeColor: "rgba(220,220,220,1)",
+      strokeColor: "rgba(236,13,13,1)",
       pointColor: "rgba(220,220,220,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
       pointHighlightStroke: "rgba(220,220,220,1)",
-      data: [random(), random(), random(), random(), random(), random(), random()]
+      data: [151,551,156,516,15,517,510,511,514,215,15,591,252,525,105]
     }, {
       label: "Real Output",
       fillColor: "rgba(151,187,205,0)",
-      strokeColor: "rgba(151,187,205,1)",
+      strokeColor: "rgba(13,13,236,1)",
       pointColor: "rgba(151,187,205,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
       pointHighlightStroke: "rgba(151,187,205,1)",
-      data: [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75]
+      data: [15,110,115,201,215,310,351,401,145,510,551,160,615,701,751]
     }]
   };
 
-
   // draw the charts
   var myLineChart = new Chart(ctx).Line(data, options);
+
 });
 
 chart.events({
